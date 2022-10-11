431c5f7380a334a2176566327a807a317fc1042e
ShamimaNasrin <shamima.nasrion007@gmail.com>
2022-10-11 20:31:37 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './layout/Main';
+import Home from './components/Home/Home';
+import Statistics from './components/Statistics/Statistics';
+import Blog from './components/Blog/Blog';
+import About from './components/About/About';
 
 function App() {
+  const router = createBrowserRouter([
+
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>  
+        },
+        
+        {
+          path: 'statistics',
+          
+          element: <Statistics></Statistics>
+        },
+        {
+          path: '/blog',
+           element: <Blog></Blog>
+        },
+        {
+          path: '/about',
+           element: <About></About>
+        }
+        
+      ]
+    },
+    {
+      path: '*', element: <h5>page not found: 404</h5>
+    }
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div>
+        
+            <Navbar bg="dark" variant='dark' expand="lg">
+                <Container >
+                    <Navbar.Brand href="#">TechQuiz</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav
+                            className="me-auto my-2 my-lg-0"
+                            style={{ maxHeight: '100px' }}
+                            navbarScroll
+                        >
+                            <Nav.Link as={Link} to='home' >Home</Nav.Link>
+                            <Nav.Link as={Link} to='statistics'>Statistics</Nav.Link>
+                            <Nav.Link as={Link} to='blog'>Blog</Nav.Link>
+                            <Nav.Link as={Link} to='about'>About</Nav.Link>
+
+                        </Nav>
+
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+        </div>
+    );
+};
+
+export default Header;

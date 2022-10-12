0428d83eb8a2dd5deed4da9e9239f6ee9f11a926
ShamimaNasrin <shamima.nasrion007@gmail.com>
2022-10-12 12:48:28 +0600

    chart responsive

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -6,19 +6,21 @@ import './Statistics.css';
 const Statistics = () => {
     const data = useLoaderData();
     const topics = data.data;
-    
+
     return (
         <div className='chart-container'>
             <h2 className='chart-text'>Total quiz chart:</h2>
-            <div>
-            <LineChart width={500} height={400} data={topics}>
-                <Line type="monotone" dataKey="total" stroke="#707070" />
-                <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-                <XAxis dataKey="name" />
-                <YAxis />
-                <Tooltip></Tooltip>
-            </LineChart>
-            </div>
+            
+                <ResponsiveContainer width="100%" height={400}>
+                    <LineChart width={500} height={400} data={topics}>
+                        <Line type="monotone" dataKey="total" stroke="#707070" />
+                        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip></Tooltip>
+                    </LineChart>
+                </ResponsiveContainer>
+           
         </div>
     );
 };

1a6c8357305ecd755e148bc6b2034bd3ef81b840
ShamimaNasrin <shamima.nasrion007@gmail.com>
2022-10-12 17:58:54 +0600

    eye icon

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import './Questions.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
 
-const Questions = ({ques}) => {
+
+const Questions = ({ ques }) => {
     const { correctAnswer, id, options, question } = ques;
     ///console.log(ques);
+
+    const eyeHandler = id => {
+        console.log(correctAnswer);
+    }
     return (
         <div>
             <div className="ques-card">
                 <h4>Quiz: {question}</h4>
-                
+
                 {
                     options.map(option => <div className='option-div'>
                         <p>{option}</p>
-                    </div> )
+                    </div>)
                 }
+
+                <FontAwesomeIcon onClick={() => eyeHandler(id)} className='eye-icon' icon={faEye}></FontAwesomeIcon>
             </div>
 
         </div>
